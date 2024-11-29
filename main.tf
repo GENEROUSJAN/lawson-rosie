c7ba7d38d37edcc86183931bd672b215f7b094ce
jan <jan@generouscloud.online>
2024-11-29 22:30:47 +0100

    second commit

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "google_compute_instance_group" "test" {
   name        = "terraform-rosie"
   description = "Terraform test instance group"
   zone        = "us-central1-a"
-  network     = google_compute_network.default.id
+  network     = google_compute_network.devs-436319.id
 }
